8da624f34fa6a58add084dfffdb3749c9c4d79a2
charlinst <charlim001@gmail.com>
2021-09-10 00:03:39 -0300

    Domain model, database seed

--- backend/src/main/java/com/devSuperior/Dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devSuperior/Dsvendas/entities/Seller.java
@@ -1,10 +1,28 @@
 package com.devSuperior.Dsvendas.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sellers")
 public class Seller {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 
+	
+	@OneToMany(mappedBy = "seller")
+	private List<Sale> sales = new ArrayList<>();
+	
 	public Seller() {
 		}
 
@@ -27,6 +45,10 @@ public class Seller {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public List<Sale> getSales() {
+		return sales;
 	}
 	
 }
